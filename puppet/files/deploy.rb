10dea0b02eaeb776a0ce8b80021abd388d4498a3
Halvard Skogsrud <halvard@skogsrud.com>
2011-05-18 21:09:28 +1000

    Set up puppet config for app servers, Go config and MCollective deployment agent

--- puppet/files/deploy.rb
+++ puppet/files/deploy.rb
@@ -11,7 +11,14 @@ module MCollective
 
       action "war" do
         validate :source, String
-
+        temp_file = '/tmp/companyNews.war'
+        tomcat_webapps_dir = '/var/lib/tomcat6/webapps'
+        run("rm #{temp_file}*")
+        run("wget --output-document=#{temp_file} #{request[:source]}")
+        run("service tomcat6 stop")
+        run("rm -rf #{tomcat_webapps_dir}/*")
+        run("cp #{temp_file} #{tomcat_webapps_dir}")
+        run("service tomcat6 start")
       end
     end
   end
--- puppet/manifests/app.pp
+++ puppet/manifests/app.pp
@@ -1,7 +1,41 @@
 import "base.pp"
 
 class app inherits base {
+  $prevayler_path = "/var/db/prevayler"
+  
+  group { 'tomcat': }
 
+  user { 'tomcat':
+    require => Group['tomcat'],
+  }
+  
+  package { "tomcat6":
+    ensure => present,
+    require => User['tomcat']
+  }
+  
+  service { "tomcat6":
+    enable => true,
+    hasstatus => true,
+    hasrestart => true,
+    ensure => running,
+    require => Package["tomcat6"]
+  }
+  
+  file { "/var/db/prevayler":
+    ensure => directory,
+    owner => "tomcat",
+    group => "tomcat",
+    require => Package["tomcat6"]
+  }
+  
+  file { "/etc/tomcat6/tomcat6.conf":
+    content => template("tomcat6.conf.erb"),
+    owner => "tomcat",
+    group => "tomcat",
+    require => [ Package["tomcat6"], File["/var/db/prevayler"] ],
+    notify => Service['tomcat6']
+  }
 }
 
 include app
